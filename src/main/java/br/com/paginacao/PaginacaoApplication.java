b672eabdbf9d436d46d03510e19fd21f956d5e51
Edson Minoru Yamazaki <edson.minoru@muralis.com.br>
2022-07-25 16:29:06 -0300

    Projeto Inicial

---
+++ src/main/java/br/com/paginacao/PaginacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.paginacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginacaoApplication.class, args);
+	}
+
+}
